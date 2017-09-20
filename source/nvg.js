748e8a9e226d140445b3f7d6d0c76ca15ab89ee9
salvachz <chavez@navegg.com>
2017-09-20 01:41:46 -0300

    preview feature

--- source/nvg.js
+++ source/nvg.js
@@ -1,3 +1,5 @@
+var Navegg
+
 (function(n, v, g) {
   "use strict"
   var a, b, o
@@ -16,8 +18,56 @@
 })(window, document, "https://tag.navdmp.com/universal.min.js")
 
 window.naveggReady = window.naveggReady || []
-var Navegg
 
 window["nvg" + INSTALL_OPTIONS.account] = new Navegg({
-  acc: INSTALL_OPTIONS.account
+  acc: INSTALL_OPTIONS.account,
+  origin: "cf"
 })
+
+window.INSTALL_SCOPE = {
+
+  nvg_preview_div: null,
+
+  errorTimeout: null,
+
+  timeout: null,
+
+  options: null,
+
+  triggerTimeout: function triggerTimeout () {
+    window.clearTimeout(window.INSTALL_SCOPE.timeout)
+    window.INSTALL_SCOPE.timeout = window.setTimeout(window.INSTALL_SCOPE.validateTag, 1500)
+  },
+
+  showErrorMsg: function showErrorMsg () {
+    window.INSTALL_SCOPE.nvg_preview_div.style.background = "#F00"
+    window.INSTALL_SCOPE.nvg_preview_div.innerHTML = "Please, put a Navegg valid Account!"
+  },
+
+  updateAccount: function updateAccount (options) {
+    window.INSTALL_SCOPE.options = options
+    window.clearTimeout(window.INSTALL_SCOPE.timeout)
+    if (window.INSTALL_SCOPE.nvg_preview_div == null) {
+      window.INSTALL_SCOPE.nvg_preview_div = document.createElement("div")
+      window.INSTALL_SCOPE.nvg_preview_div.style.cssText = "left:0;position:fixed;width:40%;height:80px;padding:10px 0;background:#65cc9a;opacity:0.8;top:15%;margin-left:30%;color:#FFF;text-align:center"
+      document.body.appendChild(window.INSTALL_SCOPE.nvg_preview_div)
+    }
+    window.INSTALL_SCOPE.nvg_preview_div.innerHTML = "Loading..."
+    window["nvg" + window.INSTALL_SCOPE.options.account] = new Navegg({
+      acc: window.INSTALL_SCOPE.options.account,
+      origin: "cf"
+    })
+    window.naveggReady.push(window.INSTALL_SCOPE.triggerTimeout)
+    window.clearTimeout(window.INSTALL_SCOPE.errorTimeout)
+    window.INSTALL_SCOPE.errorTimeout = window.setTimeout(window.INSTALL_SCOPE.showErrorMsg, 5000)
+  },
+
+  validateTag: function validateTag() {
+    window.clearTimeout(window.INSTALL_SCOPE.errorTimeout)
+    if (window.INSTALL_SCOPE.options.account && window["nvg" + window.INSTALL_SCOPE.options.account].usr) {
+      window.INSTALL_SCOPE.nvg_preview_div.style.background = "#65cc9a"
+      window.INSTALL_SCOPE.nvg_preview_div.innerHTML = "The selected Account is valid!</br></br>Acc ID:<b>" + window.INSTALL_SCOPE.options.account + "</b>"
+    }
+    else window.INSTALL_SCOPE.showErrorMsg()
+  }
+}
