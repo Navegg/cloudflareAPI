486b94d60b9bbd01104f315da0676fa5fa8d5d53
Salvador Chavez <chavez@navegg.com>
2018-03-20 18:06:59 -0300

    CF fix

--- source/nvg.js
+++ source/nvg.js
@@ -1,21 +1,23 @@
-"use strict"
+(function() {
+  'use strict'
 
-var element, firstScript
-var Navegg
+  var element, firstScript
+  var Navegg
 
-element = document.createElement("script")
-element.src = "https://tag.navdmp.com/universal.min.js"
-firstScript = document.getElementsByTagName("script")[0]
-firstScript.parentNode.insertBefore(element, firstScript)
-window.Navegg = window.Navegg || function (parms) {
-  window.Navegg.q = window.Navegg.q || []
-  window.Navegg.q.push([this, parms])
-}
+  element = document.createElement("script")
+  element.src = "https://tag.navdmp.com/universal.min.js"
+  document.head.appendChild(element)
+  window.Navegg = window.Navegg || function (parms) {
+    window.Navegg.q = window.Navegg.q || []
+    window.Navegg.q.push([this, parms])
+  }
 
-window.naveggReady = window.naveggReady || []
-window["nvg" + INSTALL_OPTIONS.account] = new Navegg({
-  acc: INSTALL_OPTIONS.account,
-  origin: "cf"
-})
+  window.naveggReady = window.naveggReady || []
+  if(INSTALL_OPTIONS.account) {
+    window["nvg" + INSTALL_OPTIONS.account] = new window.Navegg({
+      acc: INSTALL_OPTIONS.account,
+      origin: "cf"
+    })
+  }
 
-
+}())
